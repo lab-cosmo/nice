f12f658dd33e18161370ae5acdececf4e89762fb
serfg <sergei.pozdnyakov@gmail.com>
2020-09-29 12:47:58 +0200

    adding installing requirements option to setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
 from setuptools import setup, Extension, find_packages
 from Cython.Build import cythonize
 
+with open('requirements.txt', 'r') as f:
+    requirements = [
+        line.strip() for line in f if not line.strip().startswith('#')
+    ]
+
 extensions = [
     Extension("nice.*", ["nice/*.pyx"],
               extra_compile_args=['-O3', '-fopenmp'],
@@ -9,6 +14,7 @@ extensions = [
 setup(
     name='nice',
     packages=find_packages(),
+    install_requires=requirements,
     ext_modules=cythonize(extensions),
     zip_safe=False,
 )
