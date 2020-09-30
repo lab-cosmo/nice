3a26ddc235d41a0e42991ccd8ff47e96bb128e6c
serfg <sergei.pozdnyakov@gmail.com>
2020-09-30 08:11:40 +0200

    switching theme

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
-
+import sphinx_rtd_theme
 # -- Project information -----------------------------------------------------
 
 project = 'NICE'
@@ -27,7 +27,11 @@ author = 'Jigyasa Nigam, Sergey Pozdnyakov, Michele Ceriotti'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+
+
+
 extensions = [
+     "sphinx_rtd_theme"
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -44,7 +48,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
