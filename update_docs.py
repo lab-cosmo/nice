b783d4605a659e0611bec2b20c81e3cee3e6cfab
serfg <sergei.pozdnyakov@gmail.com>
2020-09-30 09:49:10 +0200

    fix

--- update_docs.py
+++ update_docs.py
@@ -4,9 +4,11 @@ os.chdir("./docs")
 os.system("make html")
 os.chdir("../")
 os.system("git checkout gh-pages")
+os.system("git rm *")
 os.system("cp -r ../build/* .")
 with open(".nojekyll", "w") as f:
     pass
+
 os.system("git add *")
 os.system("git commit -m 'automatic docs build'")
 os.system("git push")
