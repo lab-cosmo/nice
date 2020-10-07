001f2da4efb2e3ebedde6bdd21faf5f2f7599ac0
serfg <sergei.pozdnyakov@gmail.com>
2020-10-07 18:05:04 +0200

    restructuring

--- nice/utilities.py
+++ nice/utilities.py
@@ -1,6 +1,6 @@
 import tqdm
 import numpy as np
-
+import nice.rascal_coefficients
 
 def make_structural_features(features, structures, show_progress=True):
     all_species = []
@@ -51,3 +51,47 @@ def make_structural_features(features, structures, show_progress=True):
             current_positions[specie] += num_atoms_now
 
     return result
+
+def get_spherical_expansion(structures, rascal_hypers, task_size = 100, num_threads = None, show_progress = True):  
+    hypers = copy.deepcopy(rascal_hypers)
+    
+    if ('expansion_by_species_method' in hypers.keys()):
+        if (hypers['expansion_by_species_method'] != 'user defined'):
+            raise ValueError("for proper packing spherical expansion coefficients into [env index, radial/specie index, l, m] shape output should be uniform, thus 'expansion_by_species_method' must be 'user defined'")
+            
+    hypers['expansion_by_species_method'] = 'user defined'
+    
+    
+    all_species = []
+    for structure in structures:
+        all_species.append(structure.get_atomic_numbers())
+    all_species = np.concatenate(all_species, axis = 0)
+    species = np.unique(all_species)
+    all_species = all_species.astype(np.int32)
+    species = species.astype(np.int32)
+    if ('global_species' not in hypers.keys()):
+        hypers['global_species'] = [int(specie) for specie in species]
+    else:
+        for specie in species:
+            if (specie not in hypers['global_species']):
+                warnings.warn("atom with type {} is presented in the dataset but it is not listed in the global_species, adding it".format(specie))
+                hypers['global_species'].append(int(specie))
+            
+        species = np.array(hypers['global_species']).astype(np.int32)
+                
+      
+                              
+    
+    if (num_threads is None):
+        num_threads = cpu_count()
+    
+    p = Pool(num_threads) 
+    tasks = []
+    for i in range(0, len(structures), task_size):
+        tasks.append([structures[i : i + task_size], hypers, len(species)])  
+        
+    result = [res for res in tqdm.tqdm(p.imap(get_rascal_coefficients_stared, tasks), total = len(tasks), disable = not show_progress)]
+    p.close()
+    p.join()
+    result = np.concatenate(result, axis = 0)
+    return split_by_central_specie(all_species, species, result, show_progress = show_progress)
