a2acb1a877e7150b973ec15a07f07793d7d7a402
serfg <sergei.pozdnyakov@gmail.com>
2020-10-08 17:54:04 +0200

    fix

--- update_docs.py
+++ update_docs.py
@@ -54,3 +54,20 @@ for name in names:
     
 os.chdir('../..')
 
+os.system("rm -r ../build/*")
+os.chdir("./docs")
+os.system("sphinx-apidoc -f -o . ../nice")
+os.system("make html")
+os.chdir("../")
+os.system("git checkout -f gh-pages")
+os.system("git rm -r *")
+os.system("cp -r ../build/html/* .")
+with open(".nojekyll", "w") as f:
+    pass
+
+os.system("git add *")
+os.system("git add .nojekyll")
+os.system("git commit -m 'automatic docs build'")
+os.system("git push")
+os.system("git checkout master")
+
