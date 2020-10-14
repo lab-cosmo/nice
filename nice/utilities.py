93a6f04d45477b6191c8c29c27cbad2f1729b79c
serfg <sergei.pozdnyakov@gmail.com>
2020-10-14 19:39:33 +0200

    fix docstrings.py

--- nice/utilities.py
+++ nice/utilities.py
@@ -7,7 +7,7 @@ import warnings
 
 
 def get_all_species(structures):
-    ''' returning all unique atomic species among the structures
+    ''' getting all unique atomic species among the structures
     
     Args:
         structures: list of ase atoms objects
@@ -27,13 +27,13 @@ def get_all_species(structures):
 
 
 def get_compositional_features(structures, all_species):
-    ''' returning compositional features suitable for linear regression which contains information
+    ''' getting compositional features suitable for linear regression which contains information
     about the number of atoms with particular species in the structure
     
     Args:
         structures: list of Ase atoms objects
-        all_species: numpy array with ints of all unique species in the dataset. 
-        If all species argument is the same for several calls of this function, resulting 
+        all_species: numpy array with ints of all unique species in the dataset. \
+        If all species argument is the same for several calls of this function, resulting \
         blocks of compositional features are guaranteed to be consisted with each other
         
     Return:
@@ -54,17 +54,21 @@ def get_spherical_expansion(structures,
                             task_size=100,
                             num_threads=None,
                             show_progress=True):
-    '''returning spherical expansion coefficients
+    '''getting spherical expansion coefficients
     
-     Args:
-        structures: list of Ase atoms objects
-        rascal_hypers: dictionary with parameters for librascal controlling spherical expansion
-        all_species: numpy array with ints of all unique species in the dataset. 
-        If all species argument is the same for several calls of this function, resulting 
+    Args:
+        structures: list of Ase atoms objects        
+        rascal_hypers: dictionary with parameters for librascal controlling spherical expansion        
+        all_species: numpy array with ints of all unique species in the dataset. \
+        If all species argument is the same for several calls of this function, resulting \
         blocks of spherical expansion coefficients are guaranteed to be consisted with each other
-        
+        task_size: number of structures in chunk for multiprocessing
+        num_threads: number of threads in multiprocessing. If None than all available \
+        (multiprocessing.cpu_count()) threads are used
+        show_progress: whether or not show progress via tqdm
+       
     Return:
-        dictionary in which keys are elements of all_speceis, and entries are numpy arrays with indexing
+        dictionary in which keys are elements of all_speceis and entries are numpy arrays with indexing
         [environmental index, radial basis/neighbor specie index, lambda, m] with spherical expansion coefficients for 
         environments around atoms with specie indicated in key. Coefficients are stored from the beginning,
         i. e. [:, : lambda, :(2 * lambda + 1)] elements are valid
@@ -123,20 +127,21 @@ def make_structural_features(features,
                              structures,
                              all_species,
                              show_progress=True):
-    ''' returning structural features suitable for linear regression which consist of sums 
+    ''' getting structural features suitable for linear regression which consist of sums 
      over atomic features
     
     Args:
-        features: nested dictionary with atomic features. First level keys are central species, 
-        second level keys are body orders. Entries are 2-dimensional numpy arrays. 
+        features: nested dictionary with atomic features. First level keys are central species, \
+        second level keys are body orders. Entries are 2-dimensional numpy arrays. \
         structures: list of Ase atoms objects
-        all_species: numpy array with ints of all unique species in the dataset. 
-        If all species argument is the same for several calls of this function, resulting 
-        blocks of structural features are guaranteed to be consistent with each other.        
-        If for given block of structures there are no atoms of some particular specie,
-        features dictionary still have to contain key with this specie. It should contain 
-        numpy arrays with shapes [0, number of features]. This is need to get proper placing
+        all_species: numpy array with ints of all unique species in the dataset. \
+        If all species argument is the same for several calls of this function, resulting \
+        blocks of structural features are guaranteed to be consistent with each other.   \
+        If for given block of structures there are no atoms of some particular specie,\
+        features dictionary still have to contain key with this specie. It should contain \
+        numpy arrays with shapes [0, number of features]. This is need to get proper placing\
         of features to fulfill consistency.
+        show_progress: whether or not show progress via tqdm
         
     Return:
         numpy array with shape [len(structures), number of structural features] with structural features
@@ -199,15 +204,15 @@ def transform_sequentially(nice,
     ''' transforming structures into structural features by chunks in order to use less amount of RAM
     
     Args:
-        nice: dictionary where keys are species and entries are nice transformers.
-        If you want to use single nice transformer to all environments regardless of central
-        specie just pass {key : nice_single for specie in all_species}
+        nice: dictionary where keys are species and entries are nice transformers.\
+        If you want to use single nice transformer to all environments regardless of central\
+        specie just pass {key : nice_single for specie in all_species}\
         structures: list of Ase atoms objects
-        rascal_hypers: dictionary with parameters for librascal controlling spherical expansion.
-        Should be the same as used for fitting nice transformers
+        rascal_hypers: dictionary with parameters for librascal controlling spherical expansion.\
+        Should be the same as used for fitting nice transformers\
         all_species: numpy array with ints of all unique species in the dataset. 
         block_size: size of chunks measured in number of environments
-        show_progress: whether show or not progress via tqdm
+        show_progress: whether or not show progress via tqdm
         
         
     Return:
