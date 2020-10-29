4d58740303a281e28a97a96a6b6d9dfa7b3dd64c
serfg <sergei.pozdnyakov@gmail.com>
2020-10-29 18:33:09 +0100

    selectors base

--- nice/blocks/__init__.py
+++ nice/blocks/__init__.py
@@ -3,3 +3,4 @@ from nice.blocks.expansioners import *
 from nice.blocks.purifiers import *
 from nice.blocks.grouping import *
 from nice.blocks.miscellaneous import *
+from nice.blocks.selectors import *
---
+++ nice/blocks/selectors.py
@@ -0,0 +1,82 @@
+import numpy as np
+from nice.packing import pack_dense, unpack_dense
+
+
+class InvariantsVarianceSelector:
+    def __init__(self, n_components):
+        self.n_components_ = n_components
+
+    def get_subselection(self, invariants, previous):
+        if (invariants.shape[1] <= self.n_components_):
+            return np.ones([invariants.shape[1]], dtype = np.bool), np.ones([invariants.shape[1]])
+
+        variances = np.mean(invariants * invariants, axis = 0)
+        indices = np.argsort(variances)[::-1]
+        mask = np.zeros([invariants.shape[1]], dtype = np.bool)
+        mask[indices[:self.n_components_]] = True
+        return mask, np.ones([self.n_components_], dtype = np.bool)
+
+
+#for comparing purposes
+class InvariantsRandomSelector:
+    def __init__(self, n_components):
+        self.n_components_ = n_components
+
+    def get_subselection(self, invariants, previous):
+        if (invariants.shape[1] <= self.n_components_):
+            return np.ones([invariants.shape[1]], dtype=np.bool), np.ones([invariants.shape[1]])
+
+        indices = np.random.permutation(invariants.shape[1])
+        mask = np.zeros([invariants.shape[1]], dtype=np.bool)
+        mask[indices[:self.n_components_]] = True
+        return mask, np.ones([self.n_components_], dtype=np.bool)
+
+
+class IndividualSelector:
+    def __init__(self, base):
+        self.base_ = base
+
+    def get_subselection(self, covariants, l, previous):
+        covariants_p = pack_dense(covariants, l, covariants.shape[1], covariants.shape[1])
+        previous = np.concatenate(previous, axis = 1)
+        previous_p = pack_dense(previous, l, previous.shape[1], previous.shape[1])
+
+        return self.base_.get_subselection(covariants_p, previous_p)
+
+
+class CovariantsSelector:
+    def __init__(self, base):
+        self.base_ = base
+
+    def get_subselection(self, data, previous):
+        ans = []
+        for lambd in range(data.covariants_.shape[2]):
+            if (data.actual_sizes_[lambd] > 0):
+                previous_now = [el.covariants_[:, el.actual_sizes_[lambd], lambd, :] for el in previous
+                                if el.actual_sizes_[lambd] > 0]
+
+                mask, multipliers = self.base_.get_subselection(data.covariants_[:, data.actual_sizes_[lambd], lambd, :],
+                                                              previous_now)
+                ans.append(mask, multipliers)
+            else:
+                ans.append([np.array([], dtype = bool), np.array([])])
+
+        return ans
+
+class CovariantsSelectorBoth:
+    def __init__(self, base):
+        self.base_ = base
+
+    def get_subselection(self, even, even_previous, odd, odd_previous):
+        return CovariantsSelector(self.base_).get_subselection(even, even_previous),\
+               CovariantsSelector(self.base_).get_subselection(odd, odd_previous)
+
+'''class InvariantsFPSSelector:
+    def __init__(self, n_components):
+        self.n_components_ = n_components
+
+    def get_subselection(self, invariants, previous):
+        if (invariants.shape[1] <= self.n_components_):
+            return np.ones([invariants.shape[1]], dtype=np.bool), np.ones([invariants.shape[1]])
+
+        previous = np.concatenate(previous, axis = 1)'''
